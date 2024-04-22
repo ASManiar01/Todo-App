80f865aaa38891041a15140df5bb7c55512c5eae
ASManiar01 <108590655+ASManiar01@users.noreply.github.com>
2024-04-22 20:55:47 +0500

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+let todos = [];
+
+let condition = true;
+
+while(condition)
+    {
+
+        let addTask = await inquirer.prompt(
+            [
+                {
+                    name:"todo",
+                    type:"input",
+                    message:"What do you want to add in your todo list?"
+                },
+
+                {
+                    name:"addMore",
+                    type:"confirm",
+                    message:"Are you sure you want to add more? ",
+                    default:"false"
+                }
+            ]
+        );
+
+        todos.push(addTask.todo);
+
+        condition = addTask.addMore;
+
+        console.log(todos)
+
+    }
